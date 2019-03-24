97919be0b6faab5e6aaa131503138461a4e761d8
Nik Auramenka <iammicrocontroller@gmail.com>
2019-03-25 01:24:41 +0300

    Updates

---
+++ precache-manifest.92ef370d5d959b9c3a3f17de174b9260.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "155e6a3261c4d6c1d5c0",
+    "url": "/foodapp/static/css/main.dddcb908.chunk.css"
+  },
+  {
+    "revision": "155e6a3261c4d6c1d5c0",
+    "url": "/foodapp/static/js/main.36e224b9.chunk.js"
+  },
+  {
+    "revision": "d622a6a082101a24c078",
+    "url": "/foodapp/static/js/runtime~main.72ee79ff.js"
+  },
+  {
+    "revision": "d998cb00e844bc1e0dd4",
+    "url": "/foodapp/static/js/2.2482f884.chunk.js"
+  },
+  {
+    "revision": "c4581649bccdf1c79628d4ccebca518c",
+    "url": "/foodapp/index.html"
+  }
+];
